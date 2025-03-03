aa46a599579409c4a761a277c59edb929f174fb7
LAB14_USER ON WS03 <L14X18W03@citulab.net>
2025-03-03 20:55:30 +0800

    I love offset clocks and tokens

--- oauth2login/src/main/java/com/flores/oauth2login/configuration/SecurityConfig.java
+++ oauth2login/src/main/java/com/flores/oauth2login/configuration/SecurityConfig.java
@@ -1,11 +1,19 @@
 package com.flores.oauth2login.configuration;
 
+import java.time.Duration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtTimestampValidator;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.jwt.Jwt;
+
 
 @Configuration
 @EnableWebSecurity
@@ -14,11 +22,25 @@ public class SecurityConfig {
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests.anyRequest().authenticated())
+                        authorizeRequests
+                        .requestMatchers("/user-info", "/contacts").authenticated()
+                        .anyRequest().permitAll()
+                    )
                 .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("http://localhost:8080/user-info", true))
                 .logout(logout -> logout.logoutSuccessUrl("/"))
                 .formLogin(form -> form.defaultSuccessUrl("/secured", true))
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri("https://www.googleapis.com/oauth2/v3/certs").build();
+
+        // Allow clock skew of 5 minutes
+        OAuth2TokenValidator<Jwt> withClockSkew = new JwtTimestampValidator(Duration.ofMinutes(5));
+        jwtDecoder.setJwtValidator(withClockSkew);
+
+        return jwtDecoder;
     }
 }
--- oauth2login/src/main/java/com/flores/oauth2login/controller/TestController.java
+++ oauth2login/src/main/java/com/flores/oauth2login/controller/TestController.java
@@ -1,16 +1,35 @@
 package com.flores.oauth2login.controller;
 
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.flores.oauth2login.service.GooglePeopleService;
+import com.google.api.services.people.v1.model.Person;
+
 @RestController
 public class TestController {
+    @Autowired
+    private GooglePeopleService googlePeopleService;
+
     @GetMapping("/user-info")
     public Map<String, Object> getUser(@AuthenticationPrincipal OAuth2User principal) {
         return principal.getAttributes();
+    }
+
+    @GetMapping("/contacts")
+    public List<Person> getContacts(@AuthenticationPrincipal OAuth2User principal,
+                                    @RegisteredOAuth2AuthorizedClient("google") OAuth2AuthorizedClient authorizedClient)
+                                    throws Exception {
+        String accessToken = authorizedClient.getAccessToken().getTokenValue();
+
+        return googlePeopleService.getContacts(accessToken);
     }
 }
